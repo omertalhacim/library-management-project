606079d61814034005739879df57bba1dccdaa05
OmerTalhaCim <omertalhacim@gmail.com>
2020-08-26 19:59:52 +0300

    404-not found page view added

--- src/router/index.js
+++ src/router/index.js
@@ -1,41 +1,46 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/addBook',
-    name: 'BookAddingView',
-    component: () => import('../views/BookAddingView.vue')
+    path: "/addBook",
+    name: "BookAddingView",
+    component: () => import("../views/BookAddingView.vue"),
   },
   {
-    path: '/bookList',
-    name: 'BookListView',
-    component: () => import('../views/BookListView.vue')
+    path: "/bookList",
+    name: "BookListView",
+    component: () => import("../views/BookListView.vue"),
   },
   {
-    path: '/getFromUser',
-    name: 'GetFromUserView',
-    component: () => import('../views/GetFromUserView.vue')
+    path: "/getFromUser",
+    name: "GetFromUserView",
+    component: () => import("../views/GetFromUserView.vue"),
   },
   {
-    path: '/setToUser',
-    name: 'SetToUserView',
-    component: () => import('../views/SetToUserView.vue')
-  }
-]
+    path: "/setToUser",
+    name: "SetToUserView",
+    component: () => import("../views/SetToUserView.vue"),
+  },
+  {
+    path: "*",
+    name: "404",
+    component: () => import("../views/404.vue"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/404.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <br />
+    <h1 class="d-flex justify-content-center">404 - PAGE NOT FOUND :/</h1>
+    <br />
+    <h5 class="d-flex justify-content-center">Redirecting to home page in 4 second...</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "404",
+  mounted() {
+    setTimeout(() => {
+      this.$router.push("/");
+    }, 4000);
+  },
+};
+</script>
+
+<style>
+</style>
